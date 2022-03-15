8f6964b39e461ce92e1c073033be5e7a067f4dd3
ShirokovLev <leva.shirokov@gmail.com>
2022-03-15 16:23:46 +0300

    add alt attribute

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -9,7 +9,7 @@ const Banner = ({title, description, link, linkName, picUrl, className})=>{
                 <p className="text text_size_regular text_weight_semibold text_color_white banner__description">{description}</p>
                 <a href={link} className="button button_color_white banner__button">{linkName}</a>
             </div>
-            <img className="banner__pic d-none d-md-block" src={picUrl} decoding="async" loading="lazy" alt="" />
+            <img className="banner__pic d-none d-md-block" src={picUrl} decoding="async" loading="lazy" alt="fortnite-team" />
         </div>
     )
 }
